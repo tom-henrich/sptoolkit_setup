a1be1f4fa68845a42a6b539d9f17287369b7870e
Tom Henrich <tomhenrich1011@gmail.com>
2020-09-29 12:42:12 -0400

    More updates to io_start.

--- Start_io_Hub.sh
+++ Start_io_Hub.sh
@@ -30,5 +30,5 @@ sed -i 's/<RANDOM_HEX>/'"$( openssl rand -hex 32 )"'/g' config.yaml
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 # Run the config.yaml file to start up JupyterHub: RELEASE=NAMESPACE=jhub, JUPYTER_VERSION=0.7.0
-helm upgrade --install jhub jupyterhub/jupyterhub --namespace jhub --version=0.9.1 --values config.yaml 
+helm upgrade --cleanup-on-fail --install jhub jupyterhub/jupyterhub --namespace jhub --version=0.9.1 --values config.yaml 
 
